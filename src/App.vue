e0da1785c77ce80b91f71635f20cc07adef0f985
Michael Gauthier <mikegauthier222@hotmail.com>
2024-07-08 13:41:50 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <div class="m-auto">
+      lv scheduler
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
