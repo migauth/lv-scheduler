8f64e68f3e7e8740618ee208f89b28bbe45ebde1
Michael Gauthier <mikegauthier222@hotmail.com>
2024-07-08 19:26:58 -0700

    add input and colour options button

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,27 @@ import { computed, ref } from 'vue'
 import { siteStyles } from './state.js'
 import ToDoList from './components/ToDoList.vue';
 
+// Site styles
+
 const siteClass = computed(() => {
   return `${siteStyles.value.padding} ${siteStyles.value.border} ${siteStyles.value.text} ${siteStyles.value.background} ${siteStyles.value.layout}`
 })
 
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// Colour buttons
 
+const showColorOptions = ref(false)
+
+const toggleColorOptions = () => {
+  showColorOptions.value = !showColorOptions.value
+}
+
+const buttonClass = computed(() => {
+  return `${siteStyles.value.border} ${siteStyles.value.text} ${siteStyles.value.text} ${siteStyles.value.padding}`
+})
+
+// Date stuff
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 const today = new Date();
 
@@ -25,8 +40,14 @@ const time = today.toLocaleTimeString('en-US');
     <header class="border text-6xl p-4">
       LV Scheduler
     </header>
-    <div class="text-center text-lg my-4">
-      {{ dayOfWeek }}, {{ month }} {{ dayOfMonth }} - {{ time }}
+    <div class="flex  justify-between mt-3">
+      <button @click="toggleColorOptions" :class="buttonClass"
+        class="rounded-md p-2 text-3xl sm:text-2xl mb-3 mr-0 sm:mr-3 w-full sm:w-auto">
+        <i class="fa-solid fa-caret-down" /> Color Options
+      </button>
+      <div class="text-3xl">
+        {{ dayOfWeek }}, {{ month }} {{ dayOfMonth }} - {{ time }}
+      </div>
     </div>
     <div class="flex-1 border p-4">
       <ToDoList />
@@ -35,6 +56,4 @@ const time = today.toLocaleTimeString('en-US');
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,7 +4,16 @@
 
 <template>
   <h1 class="text-4xl">To do list component</h1>
-
+  <form @submit.prevent>
+    <div class="w-full">
+      <input
+        id="textInput"
+        type="text"
+        placeholder="Type here"
+        
+      />
+    </div>
+  </form>
 </template>
 
 <style scoped>
