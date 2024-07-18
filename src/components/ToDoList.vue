33c3dbe051a5255819a70cf0cf566141eed3506b
Michael Gauthier <mikegauthier222@hotmail.com>
2024-07-17 18:04:06 -0700

    add styling and plus buttonm

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -4,21 +4,20 @@
 
 <template>
   <h1 class="text-4xl">To do list component</h1>
-  <form @submit.prevent>
-    <div class="w-full">
-      <input
-        id="textInput"
-        type="text"
-        placeholder="Type here"
-        
-      />
-    </div>
-  </form>
+  <div class="border flex">
+    <form @submit.prevent>
+      <div class="w-full">
+        <input id="textInput" type="text" placeholder="Type here" class=" bg-transparent" />
+      </div>
+    </form>
+    <button><i class="fa-solid fa-plus"></i></button>
+    
+  </div>
 </template>
 
 <style scoped>
-  li {
-    font-size: 2em;
-    border: solid white;
-  }
+li {
+  font-size: 2em;
+  border: solid white;
+}
 </style>
